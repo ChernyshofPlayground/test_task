a94c0a2b9856ea45d47e4d093b1a179f28c8d3b1
chernyshof <dmitriy.chernyshof@gmail.com>
2018-01-05 10:03:38 +0200

    Added score

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
-
 var currentPos = 0;
+var score = 0;
+
+function onClickBox(e) {
+  var x = e.pageX,
+      y = e.pageY;
+  if (x > 320 && x < 340 && y > currentPos+20
+      && y < currentPos+40) {
+    currentPos = 0;
+    score += 1;
+    var scoreEl = document.getElementById('score');
+    scoreEl.innerHTML = `${score}`;
+  }
+}
 
 function animate() {  
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');  
+  canvas.addEventListener('click', onClickBox);
   ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientWidth);  
   ctx.fillRect(320, currentPos, 20, 20);
   currentPos += 1;
   if(currentPos >= canvas.clientHeight) {
     currentPos = 0;
   }
+
   requestAnimationFrame(animate);
 }
 
